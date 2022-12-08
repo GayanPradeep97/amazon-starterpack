dab7e7929a3016f8a4bfbf2f43584703d5cb5a87
GayanPradeep97 <gayanpradeep0212@gmail.com>
2022-12-07 21:41:04 -0800

    update

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
 import css from './Products.module.css'
+import Plane from '../../assets/plane.png' 
+import {ProductsData} from '../../data/products'
+import { useState } from 'react';
+
 
 class Products extends Component {
     render() {
+
+        const [MenuProducts, setMenuProducts] = useState (ProductsData);
+
         return (
-            <div>
-                products
+            <div className={css.container}>
+                <img src={Plane} alt=""/>
+                <h1>Our Featured Products</h1>
+
+                <div className={css.products}>
+                    <ul className={css.menu}>
+                        <li>All</li>
+                        <li>Skin Care</li>
+                        <li>Conditioners</li>
+                        <li>Foundations</li>
+                    </ul>
+
+                    <div className={css.list}>
+                        {
+                            MenuProducts.map((products,i)=>(
+                                <div className={css.product}>
+                                    products
+                                </div>
+                            )
+                                
+                            )
+                        }
+                    </div>
+                </div> 
             </div>
         );
     }
